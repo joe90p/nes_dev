8e6852a12b8ea15ec39f79fae43c00849f5b4e11
joe90p <philip_standen@hotmail.co.uk>
2017-05-17 21:16:33 +0100

    added some instructions

--- nesem/main.c
+++ nesem/main.c
@@ -2,12 +2,25 @@
 #include <stdlib.h>
 void load_rom();
 unsigned char* ines_file_contents;
-unsigned char* cpu_memory;
+//unsigned char* cpu_memory;
+struct NES_CPU* cpu;
+
 void run_rom();
 
+struct NES_CPU
+{
+  unsigned char* cpu_memory;
+  unsigned char A;
+  unsigned char X;
+  unsigned char Y;
+  unsigned char status;
+  unsigned short PC;
+}; 
 
 int main(int argc, char* argv[])
 {
+  cpu = malloc(sizeof(struct NES_CPU));
+  //cpu->A = 'A';
   FILE* nes_file_ptr;
   int nes_file_length; 
   nes_file_ptr = fopen("/home/phil/test.nes", "r");
@@ -25,7 +38,7 @@ int main(int argc, char* argv[])
 
 void load_rom()
 {
-  cpu_memory = malloc(0x10000);
+  unsigned char* cpu_memory = malloc(0x10000);
   short rom_fileoffset=17;
   char flags6 = ines_file_contents[6];
   if(flags6 & 8)
@@ -37,29 +50,43 @@ void load_rom()
     cpu_memory[0x8000 + i] = ines_file_contents[rom_fileoffset + i];
     cpu_memory[0xC000 + i] = ines_file_contents[rom_fileoffset + i];
   }
+  cpu->cpu_memory=cpu_memory;
 }
 
 void run_rom()
 {
-  unsigned short program_counter = cpu_memory[0xfffc]<<8 | cpu_memory[0xfffd]>>8;
+  cpu->PC = cpu->cpu_memory[0xfffc]<<8 | cpu->cpu_memory[0xfffd]>>8;
   printf("first 3 bytes at RESET\n"); 
 
   for(int j=0; j < 3; j++)
   {
-    printf("%02x\n", cpu_memory[program_counter + j]);
+    printf("%02x\n", cpu->cpu_memory[cpu->PC + j]);
   }
 
   for(int k=0; k < 5; k++)
   {
-    switch(cpu_memory[program_counter])
+    switch(cpu->cpu_memory[cpu->PC])
     {
+      case 0x0069:
+        printf("%02x: ADC #$%02x\n", cpu->PC, cpu->cpu_memory[cpu->PC+1]);
+        cpu->A+=cpu->PC+1;
+        cpu->PC+=2;
+      case 0x0065:
+        printf("%02x: ADC $%02x\n", cpu->PC, cpu->cpu_memory[cpu->PC+1]); 
+        cpu->A+=cpu->cpu_memory[cpu->PC+1];
+        cpu->PC+=2;
+      case 0x0075:
+        printf("%02x: ADC $%02x,X\n", cpu->PC, cpu->cpu_memory[cpu->PC+1]); 
+        cpu->A+=cpu->cpu_memory[cpu->PC+1+cpu->X];
+        cpu->PC+=2;
       case 0x00d8:
-        printf("%02x: CLD\n", program_counter);
-        program_counter++;
+        printf("%02x: CLD\n", cpu->PC);
+        cpu->PC++;
         break;
       case 0x00a2:
-        printf("%02x: LDX %02x\n", program_counter, cpu_memory[program_counter+1]);
-        program_counter+=2;
+        printf("%02x: LDX %02x\n", cpu->PC, cpu->cpu_memory[cpu->PC+1]);
+        cpu->X=cpu->cpu_memory[cpu->PC+1];
+        cpu->PC+=2;
         break;
     } 
   } 

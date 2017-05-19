eae7529f0c6c609cb4623345435d1b5e60946d0a
joe90p <philip_standen@hotmail.co.uk>
2017-05-19 21:31:57 +0100

    more instructions

--- nesem/main.c
+++ nesem/main.c
@@ -53,6 +53,15 @@ void load_rom()
   cpu->cpu_memory=cpu_memory;
 }
 
+void ADC_update_status_register(unsigned char oldA)
+{
+  cpu->status|=cpu->A&128;
+  cpu->status|=((cpu->A&128)^(oldA&128))>>1;
+  cpu->status|=(cpu->A==0)<<1;
+  cpu->status|=((cpu->A&128)^(oldA&128))>>7;
+
+}
+
 void run_rom()
 {
   cpu->PC = cpu->cpu_memory[0xfffc]<<8 | cpu->cpu_memory[0xfffd]>>8;
@@ -62,23 +71,31 @@ void run_rom()
   {
     printf("%02x\n", cpu->cpu_memory[cpu->PC + j]);
   }
-
+  unsigned char oldA;
   for(int k=0; k < 5; k++)
   {
+    oldA = cpu->A;
+
     switch(cpu->cpu_memory[cpu->PC])
     {
       case 0x0069:
-        printf("%02x: ADC #$%02x\n", cpu->PC, cpu->cpu_memory[cpu->PC+1]);
-        cpu->A+=cpu->PC+1;
+        printf("%02x: ADC #$%02x\n", cpu->PC, cpu->cpu_memory[cpu->PC+1]); 
+        cpu->A+=cpu->cpu_memory[cpu->PC+1];
+        ADC_update_status_register(oldA);
         cpu->PC+=2;
       case 0x0065:
         printf("%02x: ADC $%02x\n", cpu->PC, cpu->cpu_memory[cpu->PC+1]); 
-        cpu->A+=cpu->cpu_memory[cpu->PC+1];
+        cpu->A+=cpu->cpu_memory[cpu->cpu_memory[cpu->PC+1]];
+        ADC_update_status_register(oldA);
         cpu->PC+=2;
       case 0x0075:
         printf("%02x: ADC $%02x,X\n", cpu->PC, cpu->cpu_memory[cpu->PC+1]); 
-        cpu->A+=cpu->cpu_memory[cpu->PC+1+cpu->X];
+        cpu->A+=cpu->cpu_memory[cpu->cpu_memory[cpu->PC+1]+cpu->X];
+        ADC_update_status_register(oldA);
         cpu->PC+=2;
+     case 0x006d:
+        printf("%02x: ADC $%02x%02x\n", cpu->PC, cpu->cpu_memory[cpu->PC+2], cpu->cpu_memory[cpu->PC+1]); 
+        
       case 0x00d8:
         printf("%02x: CLD\n", cpu->PC);
         cpu->PC++;
@@ -91,4 +108,5 @@ void run_rom()
     } 
   } 
 } 
+
    
